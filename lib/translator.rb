bab0c966f5825cbc256dad866adf6ec9f07c9aad
Angelina Shin Manley <ashin05@gmail.com>
2019-07-26 12:55:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,13 @@ def load_library(file_path)
 
   emoticons = YAML.load_file('file_path')
   emoticon_hash = {
-      get_meaning = {}, #keys inside get_meaning hash are japanese emoticons and point to their meanings {☜(⌒▽⌒)☞, angel}
-      get_emoticon = {}#are the english emoticons, point to their japanese equivalents {O:),"☜(⌒▽⌒)☞"}
-      }
+      "get_meaning" = {}, #keys inside get_meaning hash are japanese emoticons and point to their meanings {☜(⌒▽⌒)☞, angel}
+      "get_emoticon" = {}  #are the english emoticons, point to their japanese equivalents {O:),"☜(⌒▽⌒)☞"}
+    }
   emoticons each do | translation, array |  # translation =>[english, japanese]
     emoticon_hash["get meaning"][array[1]] = translation
     emoticon_hash["get_emoticon"][array[0]] = array[1]
-  end
+  end 
 
 emoticon_hash
 end
